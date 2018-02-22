c4a402d37195b0ec717191c8a0bec9410fcee16b
MrVorgoth <skay55@interia.pl>
2018-02-22 15:52:28 +0100

    Added full greetings section with RWD

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import Greetings from './greetings';
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <Greetings />
+      </div>
     );
   }
 }
