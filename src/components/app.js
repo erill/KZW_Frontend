a5f83e6d37ee752d96fa2ccca00704132a85843e
MrVorgoth <skay55@interia.pl>
2018-02-22 14:16:13 +0100

    Added full cooperation subpage with RWD

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import Cooperation from './cooperation';
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <Cooperation />
+      </div>
     );
   }
 }
