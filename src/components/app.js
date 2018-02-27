725c7147d9d3b083bc84cba1d0b098e1779bd757
MrVorgoth <skay55@interia.pl>
2018-02-27 14:10:21 +0100

    Added navigation with RWD and react burger library

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import Nav from './nav';
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <Nav />
     );
   }
 }
---
+++ src/components/nav.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import Media from "react-media";
+import { slide as Menu } from 'react-burger-menu';
+import { Container, MenuItem } from '../../style/nav';
+
+export default class Nav extends Component {
+  render() {
+    var styles = {
+      bmBurgerButton: {
+        position: 'fixed',
+        width: '36px',
+        height: '30px',
+        left: '36px',
+        top: '36px'
+      },
+      bmBurgerBars: {
+        background: '#373a47',
+        transition: 'all .5s'
+      },
+      bmCrossButton: {
+        height: '24px',
+        width: '24px'
+      },
+      bmCross: {
+        background: '#BDC3C7'
+      },
+      bmMenu: {
+        background: '#373A47',
+        padding: '2.5em 1.5em 0',
+        fontSize: '1.15em',
+        overflow: 'none'
+      },
+      bmMorphShape: {
+        fill: '#373A47'
+      },
+      bmItemList: {
+        color: '#B8B7AD',
+        padding: '0.8em'
+      },
+      bmOverlay: {
+        background: 'rgba(0, 0, 0, 0.3)'
+      }
+    }
+
+    return (
+      <Media query="(max-width: 966px)">
+        {matches =>
+          matches ? (
+            <Menu styles={ styles } width={ '250px' }>
+              <MenuItem href="/" hamburger>Strona główna</MenuItem>
+              <MenuItem href="/" hamburger>O projekcie</MenuItem>
+              <MenuItem href="/" hamburger>Współpraca</MenuItem>
+              <MenuItem href="/" hamburger>Zapisy</MenuItem>
+              <MenuItem href="/" hamburger>Galeria</MenuItem>
+              <MenuItem href="/" hamburger>Kontakt</MenuItem>
+            </Menu>
+          ) : (
+            <Container>
+              <MenuItem href="/">Strona główna</MenuItem>
+              <MenuItem href="/">O projekcie</MenuItem>
+              <MenuItem href="/">Współpraca</MenuItem>
+              <MenuItem href="/">Zapisy</MenuItem>
+              <MenuItem href="/">Galeria</MenuItem>
+              <MenuItem href="/">Kontakt</MenuItem>
+            </Container>
+          )
+        }
+      </Media>
+    );
+  }
+}
