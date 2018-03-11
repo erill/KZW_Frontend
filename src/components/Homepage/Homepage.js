f1aea46ead6a0ebc5064ef467259bd0608407b3c
Magda Zielonka <magdalenazielonka22@gmail.com>
2018-03-11 20:15:40 +0100

    added team section

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import Greetings from '../Greetings/greetings';
 import TrainingMetamorphosis from '../Training/training';
+import Team from '../Team/team';
 
 export default class Homepage extends Component {
   render() {
@@ -9,6 +10,7 @@ export default class Homepage extends Component {
       <div>
         <Greetings />
         <TrainingMetamorphosis />
+        <Team />
       </div>
     );
   }
